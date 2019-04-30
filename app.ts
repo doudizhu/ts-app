1179c9c174c2c18733fa2426a67f6da64cfe616e
lipeng <lipeng@daokoudai.com>
2019-04-30 18:13:31 +0800

    4.TypeScript-数组-元组和枚举类型

--- app.ts
+++ app.ts
@@ -1,3 +1,4 @@
+/*
 // 基本数据类型
 // let num = 25 // 整型
 // let flo = 25.5 // 浮点类型
@@ -25,6 +26,31 @@ let isLogin = false // let isLogin:boolean=false
 // string
 let str: string = 'hello world'
 
-let anything;
+let anything; // let anything: any
 anything = 25
 anything = 'hello' // 不报错
+*/
+
+
+
+/*
+// 数组 元组 枚举
+// let names:Array<string> = ['henry','bucky']
+// names[0] = 'hello'
+// names = ['hello']
+// console.log(names)
+// let numbers:Array<number> = [1,2] // 等价于：let numbers:number[] = [1,2]
+// let anyArray: any[] = ['hehe',1,false]
+
+// 元组
+// let colors: [string,number] = ['hello',99]
+
+// enum
+enum Color{
+    Black, // 0
+    Yellow = 100, // 1
+    Red,
+}
+let myColor: Color = Color.Red
+console.log(myColor) 
+*/
