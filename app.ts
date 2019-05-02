ab97f74fc19514cc85d4480dae535d718a948372
lipeng <lipeng@daokoudai.com>
2019-05-02 20:45:40 +0800

    12.TypeScript-初识命名空间namespace

--- app.ts
+++ app.ts
@@ -290,3 +290,24 @@ let person = new Person()
 console.log(Person.PI)
 console.log(Person.calcCircle(8))
 */
+
+
+// namespace: 命名空间
+namespace MyMath{
+    export const PI = 3.14
+
+
+    export function sumValue(num1:number,num2:number):number{
+        return num1 + num2
+    }
+
+    export function calcCircle(value:number){
+        return value * PI
+    }
+}
+
+const PI = 2.88
+console.log(MyMath.sumValue(5,10))
+console.log(MyMath.calcCircle(25))
+console.log(PI)
+console.log(MyMath.PI)
