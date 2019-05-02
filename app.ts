cad43cb43433460439f98e115554238da8fa42b9
lipeng <lipeng@daokoudai.com>
2019-05-02 19:18:42 +0800

    11.TypeScript-set和get和static

--- app.ts
+++ app.ts
@@ -189,7 +189,7 @@ console.log(handleCount)
 */
 
 
-
+/*
 // class 类（属性，方法）
 class Person{
     name: string // 公开的，当前类，或被继承子类，或类外部都可用，等价于public name: string,
@@ -253,3 +253,40 @@ console.log(misterwu)
 misterwu.studentPrint()
 misterwu.printAge(32)
 misterwu.printName('ddd')
+*/
+
+
+
+
+
+/*
+// 11.TypeScript-set和get和static
+// 1.class set get修饰词 用于隔离私有属性 和 可公开属性
+// 2.class 静态属性和方法
+class Person{
+    private _name: string = '米斯特吴'
+    static PI: number = 3.14
+
+    static calcCircle(value:number):number{
+        return this.PI * value
+    }
+    
+    // 私有属性赋值
+    set setName(value:string){
+        this._name = value
+    }
+    // 私有属性取值
+    get getName(){
+        return this._name
+    }
+}
+
+let person = new Person()
+// console.log(person.getName)
+// person.setName = '米修在线'
+// console.log(person.getName)
+// console.log(person.PI)
+
+console.log(Person.PI)
+console.log(Person.calcCircle(8))
+*/
