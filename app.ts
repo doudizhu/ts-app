327fe4410b5a584bf60422d63b94276a456edd4a
lipeng <lipeng@daokoudai.com>
2019-05-03 15:47:26 +0800

    17.TypeScript-接口继承及类的实现

--- app.ts
+++ app.ts
@@ -363,7 +363,7 @@ console.log(MyMath.Circle.calcCircle(8))
 // console.log(sum(8,12))
 
 
-
+/*
 // 16.TypeScript-初识interface接口的用法
 // interface 接口
 interface Person{
@@ -400,3 +400,49 @@ let Person2: Person2 = {
     name: '米斯特吴',
     age: 28,
 }
+*/
+
+
+
+// 17.TypeScript-接口继承及类的实现
+// interface 接口
+interface PersonInterface{
+    name: string,
+    age: number, // :号，必须要写的
+    sex?: string, // ?: 可选
+    readonly salary: number, // 只读，不能修改
+    [propName:string]:any,
+    greet():void,
+}
+interface StudentInterface{
+    id: number
+    course: string
+}
+// 接口应用到class中
+class People implements PersonInterface,StudentInterface{
+    name: string = '米斯特吴'
+    age: number = 31
+    salary: number = 8000
+    id: number = 101
+    course: string = 'it'
+    greet(){
+        console.log('hello world')
+    }
+}
+// interface接口的继承
+interface Employee extends PersonInterface{
+    work: string
+}
+
+const employee: Employee = {
+    name: '米斯特吴',
+    age: 32,
+    salary: 7000,
+    greet(){
+        console.log('hello world')
+    },
+    sex: '男',
+    work: '前端开发',
+}
+
+console.log(employee)
